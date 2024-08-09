0f3d8ddf5ba5629a4ed639ecaca77066575acee6
HomeGravity <mycoding467@gmail.com>
2024-08-09 17:26:21 +0900

    코드 리펙토링

--- async_hanspell/spell_checker.py
+++ async_hanspell/spell_checker.py
@@ -4,21 +4,16 @@ import re
 from cachetools import TTLCache
 from urllib import parse
 import time
-from collections import OrderedDict
-import xml.etree.ElementTree as ET
 
 from . import __version__
-from .headers import *
-from .check_result import CheckResult
+from .spell_checker_parser import SpellParser
 from .checked import Checked
+from .headers import *
 from .check_python_version import check_python_version
 
 # 토큰 요청을 줄이기 위해 캐시로 저장합니다.
 cache = TTLCache(maxsize=10, ttl=3600)
 
-def remove_tag(text):
-    # 정규 표현식: '<...>'와 그 안의 내용을 모두 삭제합니다.
-    return re.sub(r'<[^>]*>', '', text, count=1)
 
 class AsyncSpellChecker:
     def __init__(self) -> None:
@@ -37,24 +32,15 @@ class AsyncSpellChecker:
         # 세션 생성
         self.session = aiohttp.ClientSession()
         
-    # 토큰을 생성합니다.
-    async def _create_token(self):
-        self.token = await self._get_token()
+        # Parse 생성
+        self.speel_parser = SpellParser()
+        
     
-    async def _read_token(self):
-        # 토큰이 있는경우 > 계속 사용
-        try:
-            self.token = cache.get('PASSPORT_TOKEN')
-            
-        # 토큰이 없는경우 > 새로 생성
-        except KeyError:
-            self.token = await self._get_token()
-    
-    # 토큰 값 업데이트 및 재사용 홤수
+    # 토큰 초기화
     async def initialize_token(self):
         # 토큰을 생성합니다.
-        await self._create_token() if self.token is None else await self._read_token() # 토큰을 불러옵니다.
-    
+        self.token = await self._get_token()
+        
     # 토큰 생성을 요청합니다.
     async def _get_token(self):
         # 토큰 요청
@@ -73,7 +59,7 @@ class AsyncSpellChecker:
                 token_update  = parse.unquote(match.group(1))
                 cache['PASSPORT_TOKEN'] = token_update
                 
-            return token_update 
+                return token_update 
 
 
     # 맞춤법 검사기 요청 함수입니다.
@@ -101,9 +87,15 @@ class AsyncSpellChecker:
 
         return updated_results  # 여러 결과를 리스트 형태로 반환
 
+    def _results_output(self, texts, is_output):
+        if is_output:
+            print(texts[0]) if len(texts) == 1 else print(texts)
+            return None
+        
+        return None
     
     # 매개변수로 받은 텍스트를 맞춤법 검사를 진행합니다.
-    async def spell_check(self, text, async_delay):
+    async def spell_check(self, text, async_delay, is_output=False):
         # 필요한 리스트 초기화
         spell_check_results = {
             "spell_checked": [],
@@ -115,11 +107,11 @@ class AsyncSpellChecker:
         
         # 리스트나 튜플을 texts 변수에 할당, 단일 텍스트를 리스트로 변환
         texts = text if isinstance(text, (list, tuple)) else [text]
+        texts_len = len(texts)
         
         for idx, txt in enumerate(texts):
             if len(txt) < 500: # 네이버 맞춤법 검사기 최대로 지원하는 글자수.
                 start_time = time.time()
-                await self.initialize_token()  # 반복할때마다 토큰을 재초기화 (한번 초기화하면 캐시에 저장된게 불러와짐.)
                 spell_check_results["spell_checked"].append(await self._get_response(txt))  # 응답 가져와서 저장
                 spell_check_results["passed_time"].append(time.time() - start_time)
                 spell_check_results["original_text"].append(txt)
@@ -128,90 +120,21 @@ class AsyncSpellChecker:
                 spell_check_results["spell_checked_error"][idx] = Checked(result=False)
                 
             # 1 보다 높으면.
-            if len(texts) > 1:
+            if texts_len > 1:
                 await asyncio.sleep(async_delay)  # 대기
 
 
         # 동기적으로 각 텍스트에 대해 파싱 수행
-        parsed_results = [self._parse(spell_text, original_text, passed_time) for (spell_text, original_text, passed_time) in zip(spell_check_results["spell_checked"], spell_check_results["original_text"], spell_check_results["passed_time"])]
+        parsed_results = [self.speel_parser.parse(spell_text, original_text, passed_time) for (spell_text, original_text, passed_time) in zip(spell_check_results["spell_checked"], spell_check_results["original_text"], spell_check_results["passed_time"])]
         
         
         # 글자수가 500 글자가 넘어간거는 오류로 처리하고 동기적으로 삽입
         updated_results = self._insert_spell_checked_errors(spell_check_results, parsed_results)
 
+        self._results_output(updated_results, is_output)
         return self._process_results(updated_results)
 
 
-    def _parse(self, data, text, passed_time):
-        html = data['message']['result']['html']
-        result = {
-            'result': True,
-            'original': text,
-            'checked': self._remove_tags(html),
-            'errors': data['message']['result']['errata_count'],
-            'time': passed_time,
-            'words': OrderedDict(),
-        }
-        
-        words = []
-        self._extract_words(words, html)
-        return self._check_words(result, words)  # 비동기 체크 호출
-    
-    def _remove_tags(self, text):
-        text = '<content>{}</content>'.format(text).replace('<br>','')
-        result = ''.join(ET.fromstring(text).itertext())
-
-        return result
-        
-    def _extract_words(self, words, html):
-        items = self._replace_tags(html).split(' ')
-        tmp = ''
-        for word in items:
-            if tmp == '' and word[:1] == '<':
-                pos = word.find('>') + 1
-                tmp = word[:pos]
-            elif tmp != '':
-                word = f'{tmp}{word}'
-            
-            if word.endswith("<end>"):
-                word = word.replace('<end>', '')
-                tmp = ''
-
-            words.append(word)
-
-    def _replace_tags(self, html):
-        return html.replace('<em class=\'green_text\'>', '<green>') \
-                .replace('<em class=\'red_text\'>', '<red>') \
-                .replace('<em class=\'violet_text\'>', '<violet>') \
-                .replace('<em class=\'blue_text\'>', '<blue>') \
-                .replace('</em>', '<end>')
-                
-    def _check_words(self, result, words):
-        # 동기적으로 각 단어를 체크합니다.
-        check_results = [self._check_word(word) for word in words]
-        
-        # 결과를 원하는 형식으로 변환
-        for word, check_result in zip(words, check_results):
-            result['words'][remove_tag(word)] = check_result
-
-        return Checked(**result)
-
-    def _check_word(self, word):
-        # 각 단어의 상태를 체크합니다.
-        
-        if word.startswith('<red>'):
-            return CheckResult.WRONG_SPELLING
-        
-        elif word.startswith('<green>'):
-            return CheckResult.WRONG_SPACING
-        
-        elif word.startswith('<violet>'):
-            return CheckResult.AMBIGUOUS
-        
-        elif word.startswith('<blue>'):
-            return CheckResult.STATISTICAL_CORRECTION
-        else:
-            return CheckResult.PASSED
 
     # 세션 종료.
     async def close(self):
--- test.py
+++ test.py
@@ -6,34 +6,34 @@ async def main1():
     SpellChecker = AsyncSpellChecker()
     await SpellChecker.initialize_token()
     
+import asyncio
+import time
+
+async def main1():
+    SpellChecker = AsyncSpellChecker()
+    await SpellChecker.initialize_token()
+    
     tasks = [
         SpellChecker.spell_check(
             text="안녕 하세요. 저는 한국인 입니다. 이문장은 한글로 작성됬습니다.", 
-            async_delay=2
+            async_delay=2,
+            is_output = True
         ),
-        
-        SpellChecker.spell_check(
-            text=["안녕 하세요. 저는 한국인 임니다. 이문잔은 한굴로 작성됬습니다."]*5, 
-            async_delay=2
-        ),
-        
-        SpellChecker.spell_check(
-            text=["안녕 하세요. 저는 한국인 임니다. 이문잔은 한굴로 작성됬습니다."]*5, 
-            async_delay=2
-        ),
-        
-        SpellChecker.spell_check(
-            text=["안녕 하세요. 저는 한국인 임니다. 이문잔은 한굴로 작성됬습니다."]*5, 
-            async_delay=2
-        )
+
     ]
 
     start_time = time.time()
-    # 비동기 작업을 병렬로 실행하고 완료된 순서대로 결과를 처리
-    for completed in asyncio.as_completed(tasks):
-        result = await completed
-        print(result)
+    
+    # asyncio.gather를 사용하여 모든 작업을 동시에 실행
+    results = await asyncio.gather(*tasks)
+    
+    # # 결과 출력
+    # for result in results:
+    #     print(result)
+        
     print(f"\n\n소요 시간: {time.time() - start_time:.4f}초")
+
+
 
     # 세션 종료
     await SpellChecker.close()
@@ -41,20 +41,21 @@ async def main1():
 async def main2():
     SpellChecker = AsyncSpellChecker()
     await SpellChecker.initialize_token() # 토큰 초기화 메소드 입니다. 반드시 호출해야합니다.
-    
+    start_time = time.time()
     word1 = await SpellChecker.spell_check(
-            text= ["이것은 비동기 마춤범 검사기 테스트 문장 임니다."] * 20,
-            async_delay=0
+            text= ["이것인 신입니다."*5000, "안녕 하세요. 저는 한국인 입니다. 이문장은 한글로 작성됬습니다"],
+            async_delay=0,
+            is_output = True
     )
-    
-    # for word in word1:
     print(word1)
+    print(f"\n\n소요 시간: {time.time() - start_time:.4f}초")
     
     # start_time = time.time()
     # for txt in ["이것은 비동기 마춤범 검사기 테스트 문장 임니다.", "안녕 하세요. 저는 한국인 입니다. 이문장은 한글로 작성됬습니다"]:
     #     word2 = await SpellChecker.spell_check(
     #             text=txt,
-    #             async_delay=0
+    #             async_delay=0,
+    #             is_output=True
     #     )
         
     #     print(word2)
